0b048e71debc0548c6df7bca21738cbcd36a31ca
Jose Zelada <josecarloszv97@gmail.com>
2026-07-04 11:45:47 +0200

    fix: remove unused firebase_core from Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/home_screen.dart';
 import 'services/update_service.dart';
 import 'widgets/update_dialog.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp();
-  } catch (e) {
-    debugPrint('[Firebase] Error al inicializar: $e');
-  }
+void main() {
   runApp(const MyApp());
 }
 
